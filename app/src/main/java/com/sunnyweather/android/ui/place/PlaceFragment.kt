03cf3d895d11d60e4ca96d558c807fe8881f37ce
ylymhux <ylymhux@hotmail.com>
2021-11-05 16:14:29 +0100

    加入天气功能

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -1,6 +1,7 @@
 package com.sunnyweather.android.ui.place
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sunnyweather.android.R
+import com.sunnyweather.android.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
 
 class PlaceFragment:Fragment() {
@@ -30,6 +32,19 @@ class PlaceFragment:Fragment() {
     @SuppressLint("FragmentLiveDataObserve")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        if (viewModel.isPlaceSaved()){
+            val place=viewModel.getSavedPlace()
+            val intent= Intent(context, WeatherActivity::class.java).apply{
+                putExtra("location_lng",place.location.lng)
+                putExtra("location_lat",place.location.lat)
+                putExtra("place_name",place.name)
+            }
+            startActivity(intent)
+            activity?.finish()
+            return
+        }
+
         val layoutManager=LinearLayoutManager(activity)
         recyclerView.layoutManager=layoutManager
         adapter=PlaceAdapter(this,viewModel.placeList)
